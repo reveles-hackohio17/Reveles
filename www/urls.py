0e72a1d0f0fa1affcf9e1ea369ecf72a3a8d1a3a
Andy Makovec <andrewmakovec@gmail.com>
2017-10-21 19:33:20 -0400

    Fixed static content references, added map view

--- www/urls.py
+++ www/urls.py
@@ -2,6 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+<<<<<<< HEAD
     url(r'^$', views.index, name='index'),
     url(r'^$map.html', views.map, name='map'),
 ]
+=======
+    url(r'^$', views.homepage, name='homepage'),
+]
+>>>>>>> 350f01929fa51bc30c894ff065e26174b5be43d8
