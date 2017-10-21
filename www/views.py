1cae32b515e679ea9feb135604d15b6c5eed06f1
Andy Makovec <andrewmakovec@gmail.com>
2017-10-21 19:58:54 -0400

    Fixed static content resolution

--- www/views.py
+++ www/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 <<<<<<< HEAD
+<<<<<<< HEAD
 def index(request):
     return render(request, 'www/index.html', {})
 
 def map(request):
     return render(request, 'www/map.html', {})
+=======
+def homepage(request):
+    return render(request, 'www/index.html', {})
+>>>>>>> 350f01929fa51bc30c894ff065e26174b5be43d8
 =======
 def homepage(request):
     return render(request, 'www/index.html', {})
