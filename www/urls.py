ed8632c1902b6f58dbede93ef787e6e28faf84ce
Your Name <nkassar0@gmail.com>
2017-10-22 04:42:52 -0400

    change

--- www/urls.py
+++ www/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.homepage, name='homepage'),
     url(r'^map/?$', views.map, name='map'),
+    url(r'^add-event/?$', views.add_event, name='add_event'),
     
 ]
--- www/views.py
+++ www/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from events.models import Event 
 from django.core import serializers
+from .forms	import EventForm
 
 # Create your views here.
 def homepage(request):
@@ -11,4 +12,20 @@ def map(request):
     events = json_serializer.serialize(Event.objects.all(), ensure_ascii=False)
     #events = Event.objects.all()
     return render(request, 'www/map.html', {'events': events})
+
+def add_event(request):
+    if request.method == "POST":
+        form = EventForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+			#try using geopy for address geocoding
+            #return redirect('map')
+    else:
+        form = EventForm()
+    return render(request, 'www/add_event.html', {'form': form})
+
+
+
 
