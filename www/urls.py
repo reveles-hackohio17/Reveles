c6a6c12e8fb42227188100dabb641a1345ee1422
Andy Makovec <andrewmakovec@gmail.com>
2017-10-21 20:31:34 -0400

    Bugfixes for map page in views, urls, and index

--- www/urls.py
+++ www/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^$map.html', views.map, name='map'),
+    url(r'^$', views.homepage, name='homepage'),
+    url(r'^$map', views.map, name='map'),
 ]
