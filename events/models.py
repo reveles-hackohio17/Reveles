3a2c245dd37e524cdae6b37d3d2fb2af7d4b6034
Your Name <nkassar0@gmail.com>
2017-10-22 06:20:40 -0400

    change

--- events/models.py
+++ events/models.py
@@ -4,18 +4,19 @@ from django.utils import timezone
 class Event(models.Model):
     author = models.ForeignKey('auth.User')
     event_name = models.CharField(max_length=200)
-    description = models.TextField()
+    general_category = models.CharField(default='Student Org', max_length=100)
+    specific_category = models.CharField(default='Engineering', max_length=100)
+    description = models.TextField(blank=True, null=True)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    longitude = models.FloatField()
-    latitude = models.FloatField()
-    created_date = models.DateTimeField(
-        default=timezone.now)
-    cost = models.TextField()
-    num_likes = models.IntegerField()
-    num_attendees = models.IntegerField()
-    address = models.TextField()
-    image = models.TextField()
+    longitude = models.FloatField(blank=True, null=True)
+    latitude = models.FloatField(blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    cost = models.FloatField(default=0)
+    num_likes = models.IntegerField(default=0)
+    num_attendees = models.IntegerField(default=0)
+    address = models.CharField(max_length=512)
+    image = models.TextField(blank=True, null=True)
     
 
     def publish(self):
