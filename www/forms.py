5e08cc9cfd21c44fa720d4de076b7c3fba3885f5
Cooper <cooperextra@gmail.com>
2017-10-22 04:22:18 -0400

    Added "add event" page, some geocoding stuff

---
+++ www/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from events.models import Event
+from functools import partial
+DateInput = partial(forms.DateInput, {'class': 'datepicker'})
+
+class EventForm(forms.ModelForm):
+    
+    start_time = forms.DateField(widget=DateInput())
+    end_time = forms.DateField(widget=DateInput())
+    class Meta:
+        model = Event
+        fields = ('event_name', 'description', 'start_time', 'end_time', 'address', 'cost', 'image')
